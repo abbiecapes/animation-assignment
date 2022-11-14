1ba8625aef6103e71c18a686bacdcef27be53825
abbiecapes <amc20fe@fsu.edu>
2022-11-14 13:35:57 -0500

    stuff

--- sketch.js
+++ sketch.js
@@ -1,33 +1,37 @@
+let img;
+let tempImg;
+let myCard;
+let bg; 
+let y = 0;
+let deck = [];
+
+function preload() {
+  bg = loadImage("small.background.5.jpg");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1280, 657,WEBGL);;
+  bg = loadImage("small.background.5.jpg");
+    for (let i = 0; i < 5; i++) {
+     let tempImg =loadImage("card"+i+"jpg");
+     let tempCard = new Card(tempImg,40,40+i*150);
+     deck.push(tempCard);
+    }; {
+      loadImage("card"+ i +".jpg", img => {
+        let tempCard = new Card(tempImg,40,40+i*150);
+        deck.push(tempCard);
+    });
+  }
 }
 
 function draw() {
-  background(125);
-  function setup() {
-    createCanvas(100, 100, WEBGL);
-  }
-  
-    rect(200, 200, 40, 50)
-    background(200);
-    //rotateZ(radians(rotationZ));
-    rotateX(radians(rotationX));
-    //rotateY(radians(rotationY));
-    box(200, 200, 200);
-    describe(`red horizontal line right, green vertical line bottom.
-      black background.`);
-  }
-  let value = 0;
- 
-    fill(value);
-    rect(25, 25, 50, 50);
-    describe(`black 50-by-50 rect turns lighter with mouse click and
-      drag until white, resets`);
-  function mouseDragged() {
-    value = value + 5;
-    if (value > 255) {
-      value = 0;
-    }
-  }
-  
+  background (bg);
+  deck.forEach(card => { 
+    myCard.display(); });
+
+    image(img, 0, 0);
+  myCard.display();
+  myCard = new Card(img, 100, 100, 100, 100);
+  location = 100, 100;
+}
 
